9645fc7b6d04e9b069cd44c3c1672203e391d198
Theo HERVEUX <hurobaki@gmail.com>
2016-11-15 15:50:06 +0100

    Client Side

---
+++ CSharp_Client/CSharp_Client/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+using chatLibrary;
+using System.Threading;
+
+namespace CSharp_Client
+{
+    public partial class Form1 : Form
+    {
+
+        public NetworkStream stream;
+        public TcpClient client;
+
+        public Form1()
+        {
+            InitializeComponent();   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient("192.168.137.129", 25565);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Le serveur distant n'est pas connecté");
+                Console.ReadLine();
+            }
+            
+
+            stream = client.GetStream();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            AuthPaquet ap = new AuthPaquet(textBox1.Text, textBox2.Text);
+
+            Paquet.Send(ap, stream);
+
+            Thread.Sleep(100);
+        }
+    }
+}
