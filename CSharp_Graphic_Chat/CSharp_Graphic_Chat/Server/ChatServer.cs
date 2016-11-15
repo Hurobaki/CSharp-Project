2e91a2c2f0d126e8fd876f4192f4a84d353748d9
IDXCEL <IDXCEL@LAPTOP-0TR12DI3>
2016-11-15 15:06:40 +0100

    Pareil que le commit d'avant

--- CSharp_Graphic_Chat/CSharp_Graphic_Chat/Server/ChatServer.cs
+++ CSharp_Graphic_Chat/CSharp_Graphic_Chat/Server/ChatServer.cs
@@ -9,11 +9,13 @@ using System.Threading;
 using Project_CSharp.Chat.Chat;
 using System.Net;
 using System.IO;
+using chatLibrary;
 
 namespace Project_CSharp.Server
 {
     class ChatServer
     {
+        /*
         static void Main(string[] args)
         {
             TcpListener ServerSocket = new TcpListener(IPAddress.Any, 25565);
@@ -27,67 +29,49 @@ namespace Project_CSharp.Server
                 client.startClient(clientSocket);
             }
         }
+        */
     }
 
     public class handleClient
     {
         TcpClient clientSocket;
-        TopicsManager tm = new TopicsManager();
         public void startClient(TcpClient inClientSocket)
         {
             this.clientSocket = inClientSocket;
-            Thread ctThread = new Thread(Chat);
+            Thread ctThread = new Thread(Auth);
             ctThread.Start();
         }
-
-        public void run()
+        private void Auth()
         {
-            Console.WriteLine("Running server");
-            while (true)
+            try
             {
-                Console.WriteLine("In the while");
-                Socket client = server.Accept();
                 
-                Console.WriteLine("New Client");
-                HandleClient hclient = new HandleClient();
-                hclient.startClient(client);
+                Paquet paquet = Paquet.Receive(clientSocket.GetStream());
+
+                if (paquet is AuthPaquet)
+                {
+                    AuthPaquet ap = (AuthPaquet)paquet;
+
+                    Console.WriteLine(ap.Id);
+                    Console.WriteLine(ap.MotDePasse);
+                }
+
+                /*while (true)
+                {
+                    BinaryReader reader = new BinaryReader(clientSocket.GetStream());
+                    String line = reader.ReadString();
+                    if (line.Equals("2"))
+                    {
+
+                    }
+                    Console.WriteLine(reader.ReadString());
+                }*/
             }
-        }
-        public static void Main(String[] args)
-        {
-            ChatServer cs = new ChatServer();
-            cs.startServer();
-            cs.run();
-        }
-    }
-    public class HandleClient
-    {
-        private Socket clientSocket;
-        private byte[] bytesFrom = new byte[10000];
-
-        public void startClient(Socket c)
-        {
-            clientSocket = c;
-            Thread clientThread = new Thread(gereClient);
-            clientThread.Start();
-        }
-
-        public void gereClient()
-        {
-         
-
-            while ((true))
+            catch (IOException e)
             {
-                try
-                {
-                    clientSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000));
-                    clientSocket.Listen(200);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(" >> " + ex.ToString());
-                }
+                Console.WriteLine("Un client a déconnecté");
             }
+
         }
     }
 }
---
+++ chatLibrary/chatLibrary/AuthPaquet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.Serialization;
+
+namespace chatLibrary
+{
+    [Serializable()]
+    public class AuthPaquet : Paquet
+    {
+        public String Id { get; private set; }
+        public String MotDePasse { get; private set; }
+
+        public AuthPaquet(String Id, String MotDePasse) : base(TypePaquet.Authentification)
+        {
+            this.Id = Id;
+            this.MotDePasse = MotDePasse;
+        }
+    }
+}
